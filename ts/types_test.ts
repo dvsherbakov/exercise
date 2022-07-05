6f556d5a1cf444cdfc0cb6fbe1b59addec997c76
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-07-05 17:50:01 +0500

    types ts probe

--- ts/types_test.ts
+++ ts/types_test.ts
@@ -16,7 +16,7 @@ class TypeSystem {
     static none: unknown = undefined
 
     //static void: void = action()
-   // static never: never = action2()
+    // static never: never = action2()
 }
 
 //const af = (arg: any): void => arg
@@ -25,9 +25,42 @@ class TypeSystem {
 TypeSystem.boolean = TypeSystem.any
 let tmp: null = null
 
-const tm: any = '7878'
+const tm: any = '787865 '
 TypeSystem.none = tm
 
 console.log(TypeSystem.none);
 
 //error('test error')
+
+
+const f0 = (): unknown => {
+    return
+}
+
+console.log('unknown', f0())
+
+type MappedType<T> = {
+    [K in keyof T]: T
+}
+
+type T1 = MappedType<number>
+
+const t1: T1 = 55;
+
+enum Berries {
+    Strawberry = 1, Raspberry, Blueberry = 4, CherryPy
+}
+
+
+const berry = Berries["Blueberry"]
+console.log(berry)
+//console.log(Berries[1])
+
+enum Stones {
+    Peach, Apricot = 'apricot', Cherry = 202, Berry, Plum = 'plum'
+}
+
+for (const st in Stones) {
+    console.log(st, Stones[st])
+
+}
