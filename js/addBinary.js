27b4c7fe4ae7079476af8e0bfc0082c7ca1031c8
Dmitry <dsherbakov@slb.com>
2023-12-27 10:05:41 +0500

    add exercises

---
+++ js/addBinary.js
@@ -0,0 +1,6 @@
+const addBinary = (a,b) => (a+b).toString(2)
+
+ console.log(addBinary(4,3))
+
+
+//Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
