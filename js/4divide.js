5fbdb545999bef0ecfa3e99e1cc077678d2506da
Dmitry <dsherbakov@slb.com>
2024-05-13 21:17:27 +0500

    divides 4

--- js/4divide.js
+++ js/4divide.js
@@ -1,8 +1,8 @@
 const mantissa = (src) => src - ~~src === 0 ? 0 : src.toString().split('.')[1].length
-const borderCondition = (src, max) => max > 20 ? (src % 10 !== 0) && (src % 5 !== 0) : (src % 10 !== 0) && (src % 4 !== 0)
+const borderCondition = (src, max) => max > 20 ? !((src % 10 === 0) || (src % 5 === 0)) : !((src % 5 === 0) || (src % 2 === 0))
 
 const fourDivide = (min, max) => {
-  const pre = Math.min(mantissa(min), mantissa(max))
+  const pre = Math.max(mantissa(min), mantissa(max))
   let bMin = Math.ceil(min * (10 ** pre))
   let bMax = Math.ceil(max * (10 ** pre))
   while (borderCondition(bMin, max)) --bMin
@@ -12,7 +12,7 @@ const fourDivide = (min, max) => {
   if (tic > 2) while (tic % 5 !== 0) ++tic
   if (bMax>(bMin + 4 * tic)) {
     tic = Math.ceil(dif / (4))
-    if (tic > 2) while (tic % 5 !== 0) ++tic
+    if (tic > 3) while (tic % 5 !== 0) ++tic
   }
 
   tic = tic / (10 ** pre)
@@ -26,7 +26,9 @@ const fourDivide = (min, max) => {
 // console.log(mantissa(2.54))
 // console.log(mantissa(4))
 
-//console.log(3.62, 22.1, fourDivide(3.62, 22.1))
+
+console.log(9.9,10, fourDivide(9.9,10))
+console.log(3.62, 22.1, fourDivide(3.62, 22.1))
 console.log(11, 14, fourDivide(11, 14))
 console.log(0.21, 0.48, fourDivide(0.21, 0.48))
 console.log(107, 408, fourDivide(107, 408))
